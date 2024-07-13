3895a67b6e10f5f3dfc03a12dc1609f253369528
wpbt <bharatthapa45@gmail.com>
2024-07-13 18:11:37 +0545

    types topic added

---
+++ types/README.md
@@ -0,0 +1,21 @@
+# Types in PHP
+
+In programming, a statement always results in a value. This value is of specific type. PHP statements can result in following types:
+
+- `null`
+- `bool`
+- `string`
+- `int`
+- `float`
+- `array`
+- `object`
+- `callable`
+- `resource`
+
+## Notes
+
+- PHP is a dynamic language, which means the type of a variable is determined at runtime. However, PHP also allows you to specify the type of a variable during its declaration.
+
+- Specifying the types in programs is good for debugging and reliability.
+
+- We will learn more about types in upcoming topics when we dive into other PHP features: functions & classes.
---
+++ types/types.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Introduction to PHP Types
+ */
+
+/**
+ * Generates a greeting message.
+ *
+ * This function takes an optional string argument representing a name and returns
+ * a greeting message. If no name is provided, it defaults to greeting a visitor.
+ *
+ * @param string $name The name of the person to greet. Default is an empty string.
+ * @return string A greeting message.
+ */
+function greet(string $name = ''): string {
+    return $name ? 'Hello, ' . $name : 'Hello Visitor';
+}
+
+// Example usage:
+echo greet( 'John' ); // Outputs: Hello, Alice
+echo greet();        // Outputs: Hello Visitor
+
+
+/**
+ * Calculates the sum of two integers.
+ *
+ * This function takes two integer arguments and returns their sum.
+ *
+ * @param int $a The first integer to be added.
+ * @param int $b The second integer to be added.
+ * @return int The sum of the two integers.
+ */
+function sumResult(int $a, int $b): int {
+    return $a + $b;
+}
+
+// Example usage:
+echo sumResult(3, 5); // Outputs: 8
