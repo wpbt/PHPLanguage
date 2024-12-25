d387da8cb42f15cabbb16faafbc0df3acf77d5f8
wpbt <bharatthapa45@gmail.com>
2024-12-25 14:10:40 +0545

    introduction chapter improved

--- introduction/introduction.php
+++ introduction/introduction.php
@@ -4,21 +4,42 @@
  */
 
 // Example of good variable names.
-$name       = '';
-$count      = 0;
+$name       = 'John Doe';
+$count      = 10;
 $colors     = array( 'Red', 'Yellos' );
 $indexes    = array( 11, 14, 99 );
+$weight     = 70.5;
 
 // defining constant
 const PI = 3.1415;
 define( 'RATE', 3.0 );
 
-# defining a class
-class ClassName {
+// printing or displaying values.
+echo PI;
+echo PHP_EOL; // prints a new line; same as: echo "\n"
+print constant( 'RATE' );
+echo "\n";
+print( "$name $count" );
+print PHP_EOL;
+print_r( $colors );
+echo "\n";
+var_dump( $indexes );
+echo PHP_EOL;
+// formatting a string:
+printf( 'Name: %s, Weight: %d(kg)', $name, $weight );
+$formatted_string = sprintf( 'Name: %s, Weight: %d(kg)', $name, $weight );
+echo "\n";
+echo $formatted_string;
 
-}
+# defining a class
+class ClassName {}
+// creating obj:
+$obj = new ClassName; // you can also use `new ClassName()`
+echo "\n";
+var_dump( $obj );
 
 // defining a function
-function functionName() {
-
-}
+function function_name() {}
+// calling function name
+echo "\n";
+function_name();
